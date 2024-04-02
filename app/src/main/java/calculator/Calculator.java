8ad25817322b414d76dae060a878d6520bdd21cd
flipperblack <marco.gomez@davinci.edu.ar>
2024-04-02 14:08:57 -0300

    feat: agregar clase calculator

---
+++ app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+
+    // TODO: feat: agregar metodo de suma de dos enteros
+
+    // TODO: feat: agregar metodo de multiplicacion de dos enteros
+
+    // TODO: feat: agregar metodo de resta de dos enteros
+
+    // TODO: feat: agregar metodo de division de dos enteros
+
+}
