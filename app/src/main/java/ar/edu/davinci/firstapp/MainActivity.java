3877e5c1a628273ce683062db3a3017698408503
flipperblack <marco.gomez@davinci.edu.ar>
2024-04-20 13:19:34 -0300

    Feat: subiendo archivos de clase 3 - B

--- app/src/main/java/ar/edu/davinci/firstapp/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/firstapp/MainActivity.java
@@ -4,6 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import calculator.Calculator;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +31,45 @@ public class MainActivity extends AppCompatActivity {
         float resultadoDivision = calculator.dividir(primero: 2, segundo: 1);
         //TODO: feat: imprimir resultado
     }
+
+    public void sumar(View view){
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.sumar);
+    }
+
+    public void restar(View view){
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.restar);
+    }
+
+    public void multiplicar(View view){
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.multiplicar);
+    }
+
+    public void dividir(View view){
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.dividir);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText("Probando pantalla");
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
 }
